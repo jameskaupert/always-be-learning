4422a19381d38adadd98d42ba4c1869e93b53b58
James Kaupert <jameskaupert@gmail.com>
2024-11-17 13:36:30 -0600

    init aws-cdk example project

---
+++ aws-cdk/static-assets-example/lib/static-assets-example-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class StaticAssetsExampleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'StaticAssetsExampleQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
