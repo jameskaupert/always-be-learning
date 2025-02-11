b775c55f0803580675befe84763d6530c200d87a
James Kaupert <jameskaupert@gmail.com>
2025-02-11 11:20:47 -0600

    start rocket game project

---
+++ godot/rocket-3d/player.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Player Node ready")
+	print("Let's do this!")
+	print(42)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
