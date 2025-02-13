26558a535d40aac87566bd9cab48170072a1d761
James Kaupert <jameskaupert@gmail.com>
2025-02-13 08:48:38 -0600

    add export annotations for thrust

--- godot/rocket-3d/player.gd
+++ godot/rocket-3d/player.gd
@@ -1,15 +1,21 @@
 extends RigidBody3D
 
+## Amount of vertical force applied during motion
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+
+## Amount of torque applied during motion
+@export var torque_thrust: float = 100.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 		
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, delta * 100.0))
+		apply_torque(Vector3(0.0, 0.0, delta * torque_thrust))
 
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -delta * 100.0))
+		apply_torque(Vector3(0.0, 0.0, -delta * torque_thrust))
 
 
 func _on_body_entered(body: Node) -> void:
