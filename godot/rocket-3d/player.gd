41ed697a403c5fb21461411583ffb5664c876af4
James Kaupert <jameskaupert@gmail.com>
2025-06-16 15:19:54 -0500

    add more tweening for crash/level success. add audio

--- godot/rocket-3d/player.gd
+++ godot/rocket-3d/player.gd
@@ -6,6 +6,11 @@ extends RigidBody3D
 ## Amount of torque applied during motion
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -19,15 +24,28 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if "hazard" in body.get_groups():
-		crash_sequence()
-	if "goal" in body.get_groups():
-		complete_level(body.file_path)
+	if is_transitioning == false:
+		if "hazard" in body.get_groups():
+			crash_sequence()
+		if "goal" in body.get_groups():
+			complete_level(body.file_path)
 
 func crash_sequence() -> void :
 	print("KABOOM!")
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true
+	explosion_audio.play()
+	var tween = create_tween()
+	tween.tween_interval(2.5)
+	tween.tween_callback(get_tree().reload_current_scene)
+	
 	
 func complete_level(next_level_file: String) -> void:
 	print("You Win!")
-	get_tree().change_scene_to_file(next_level_file)
+	set_process(false)
+	is_transitioning = true
+	success_audio.play()
+	var tween = create_tween()
+	tween.tween_interval(2.5)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+	
