77c3a5a870fbb8c517370f564d3da50c7137b0e5
James Kaupert <jameskaupert@gmail.com>
2025-06-21 17:14:23 -0500

    add rig and set up pointing based on movement

--- godot/rpg-3d/rpg-3d/player/player.gd
+++ godot/rpg-3d/rpg-3d/player/player.gd
@@ -10,9 +10,12 @@ var _look := Vector2.ZERO
 @export var mouse_sensitivity: float = 0.00075
 @export var min_boundary: float = -60
 @export var max_boundary: float = 10
+@export var animation_decay: float = 20.0
 
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
+@onready var rig_pivot: Node3D = $RigPivot
+
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -32,6 +35,7 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		look_toward_direction(direction, delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -62,4 +66,17 @@ func frame_camera_rotation() -> void:
 		)
 		
 	_look = Vector2.ZERO
+	
+func look_toward_direction(direction: Vector3, delta: float) -> void:
+	var target_transform := rig_pivot.global_transform.looking_at(
+		rig_pivot.global_position + direction,
+		Vector3.UP,
+		true
+	)
+	
+	
+	rig_pivot.global_transform = rig_pivot.global_transform.interpolate_with(
+		target_transform,
+		1.0 - exp(-animation_decay * delta)
+	)
 	
