9c08707baa1dd66f3da9b78ee7021ff400d08dbc
James Kaupert <jameskaupert@gmail.com>
2025-06-21 17:41:45 -0500

    smooth animation between idle/run

--- godot/rpg-3d/rpg-3d/player/rig.gd
+++ godot/rpg-3d/rpg-3d/player/rig.gd
@@ -3,10 +3,20 @@ class_name Rig
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
+@export var animation_speed: float = 9.0
+
 var run_path: String = "parameters/MoveSpace/blend_position"
+var run_weight_target := -1.0
+
+func _physics_process(delta: float) -> void:
+	animation_tree[run_path] = move_toward(
+		animation_tree[run_path], 
+		run_weight_target, 
+		delta * animation_speed
+		)
 
 func update_animation_tree(direction: Vector3) -> void:
 	if direction.is_zero_approx():
-		animation_tree[run_path] = -1.0
+		run_weight_target = -1.0
 	else:
-		animation_tree[run_path] = 1.0
+		run_weight_target = 1.0
