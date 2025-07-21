46973a2324c4b28d684e2ddfaef8f4e12d35a3e0
James Kaupert <jameskaupert@gmail.com>
2025-07-21 16:01:46 -0500

    more dash work for player

---
+++ godot/rpg-3d/player/dash.gd
@@ -0,0 +1,33 @@
+extends Node3D
+class_name Dash
+
+@export var player: Player
+
+@onready var timer: Timer = $Timer
+
+@export var speed_multiplier := 3.0
+
+var direction := Vector3.ZERO
+var dash_duration := 0.1
+var time_remaining := 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not timer.is_stopped():
+		return
+		
+	if event.is_action_pressed("dash"):
+		direction = player.get_movement_direction()
+		
+		if not direction.is_zero_approx():
+			player.rig.travel("Dash")
+			timer.start(1.0)
+			time_remaining = dash_duration
+
+func _physics_process(delta: float) -> void:
+	if direction.is_zero_approx():
+		return
+	player.velocity = direction * player.SPEED * speed_multiplier
+	time_remaining -= delta
+	if time_remaining <= 0:
+		direction = Vector3.ZERO
